4aeb42bc2a84b13be698e102339f6d52a04bf804
Damindu Sandaruwan <kgdsbandarak@gmail.com>
2020-04-23 22:45:34 +0530

    SideBar Animation Done

--- lib/sidebar/sidebar.dart
+++ lib/sidebar/sidebar.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:rxdart/rxdart.dart';
 
 class SideBar extends StatefulWidget {
 
@@ -9,19 +12,43 @@ class SideBar extends StatefulWidget {
 class _SideBarState extends State<SideBar> with SingleTickerProviderStateMixin<SideBar>{
   
   AnimationController _animationController;
-  final bool isSideBarOpened =true;
+
+  StreamController<bool> isSideBarOpenedStreamController;
+  Stream<bool> isSideBarOpenedStream;
+  StreamSink<bool> isSideBarOpenedSink;
+
+  //final bool isSideBarOpened =true;
   final _animationDuration = const Duration(milliseconds: 500);
 
   @override
   void initState() {
     super.initState();
     _animationController = AnimationController(vsync: this, duration: _animationDuration);
+    isSideBarOpenedStreamController = PublishSubject<bool>();
+    isSideBarOpenedStream =  isSideBarOpenedStreamController.stream;
+    isSideBarOpenedSink=isSideBarOpenedStreamController.sink;
   }
 
   @override
   void dispose() {
     super.dispose();
-    _animationController.dispose();
+    _animationController.dispose();    
+    isSideBarOpenedStreamController.close();
+    isSideBarOpenedSink.close();
+  }
+
+  void onIconPressed(){
+    final animatesdStatus=_animationController.status;
+    final isAnimationCompleted = animatesdStatus == AnimationStatus.completed;
+
+    if(isAnimationCompleted){
+      isSideBarOpenedSink.add(false);
+      _animationController.reverse();
+    }else{
+      isSideBarOpenedSink.add(true);
+      _animationController.forward();
+    }
+
   }
 
   @override
@@ -30,36 +57,47 @@ class _SideBarState extends State<SideBar> with SingleTickerProviderStateMixin<S
     final screenWidth = MediaQuery.of(context).size.width;
     
 
-    return AnimatedPositioned(
-      duration: _animationDuration,
-      top: 0,
-      bottom: 0,
-      left: isSideBarOpened ? 0 : 0,
-      right: isSideBarOpened ? 0 : screenWidth-45,
-      child: Row(
-        children: <Widget>[
-          Expanded(
-            child: Container(
-              color: Colors.red,
-            ),
-          ),
-          Align(
-            alignment: Alignment(0,-0.9),
-            child: Container(
-              width: 35,
-              height: 110,
-              color: Colors.red,
-              alignment: Alignment.centerLeft,
-              child: AnimatedIcon(
-                progress: _animationController.view,
-                icon: AnimatedIcons.menu_close,
-                color: Colors.black,
-                size: 25,
+    return StreamBuilder<bool>(
+      initialData: false,
+      stream: isSideBarOpenedStream,
+      builder: (context, isSideBarOpenedAsync) {
+        return AnimatedPositioned(
+          duration: _animationDuration,
+          top: 0,
+          bottom: 0,
+          left: isSideBarOpenedAsync.data ? 0 : 0,
+          right: isSideBarOpenedAsync.data ? 0 : screenWidth-45,
+          child: Row(
+            children: <Widget>[
+              Expanded(
+                child: Container(
+                  color: Colors.red,
+                ),
               ),
-            ),
+              Align(
+                alignment: Alignment(0,-0.9),
+                child: GestureDetector(
+                  onTap: (){
+                    onIconPressed();
+                  },
+                  child: Container(
+                    width: 35,
+                    height: 110,
+                    color: Colors.red,
+                    alignment: Alignment.centerLeft,
+                    child: AnimatedIcon(
+                      progress: _animationController.view,
+                      icon: AnimatedIcons.menu_close,
+                      color: Colors.black,
+                      size: 25,
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
-        ],
-      ),
+        );
+      }
     );
   }
 }
