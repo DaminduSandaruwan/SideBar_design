085fc36394755b62bb2e6958475bd934c940f0ae
Damindu Sandaruwan <kgdsbandarak@gmail.com>
2020-04-24 21:36:40 +0530

    Add circleavatar and divider

--- lib/sidebar/sidebar.dart
+++ lib/sidebar/sidebar.dart
@@ -65,13 +65,51 @@ class _SideBarState extends State<SideBar> with SingleTickerProviderStateMixin<S
           duration: _animationDuration,
           top: 0,
           bottom: 0,
-          left: isSideBarOpenedAsync.data ? 0 : 0,
+          left: isSideBarOpenedAsync.data ? 0 : -screenWidth,
           right: isSideBarOpenedAsync.data ? 0 : screenWidth-45,
           child: Row(
             children: <Widget>[
               Expanded(
                 child: Container(
                   color: Colors.red,
+                  child: Column(
+                    children: <Widget>[
+                      SizedBox(height: 100),
+                      ListTile(
+                        title: Text(
+                          "Developer",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 25,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        subtitle: Text(
+                          "developer@test.com",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        leading: CircleAvatar(
+                          child: Icon(
+                            Icons.perm_identity,
+                            color: Colors.white,
+                          ),
+                          radius: 35,
+                        ),
+                      ),
+                      Divider(
+                        height: 64,
+                        thickness: 0.5,
+                        color: Colors.white.withOpacity(0.5),
+                        indent: 16,
+                        endIndent: 16,
+                      ),
+
+                    ],
+                  ),
                 ),
               ),
               Align(
